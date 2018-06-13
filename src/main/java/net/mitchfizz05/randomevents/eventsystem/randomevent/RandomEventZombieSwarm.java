2321dfcd69b9a6d6404c31ad63dcabf2d272b2ce
Mitchfizz05 <me@mitchfizz05.net>
2018-06-13 17:17:24 +0930

    The zombie swarm zombies now (usually) spawn with helmets. Fixes #4

--- src/main/java/net/mitchfizz05/randomevents/eventsystem/randomevent/RandomEventZombieSwarm.java
+++ src/main/java/net/mitchfizz05/randomevents/eventsystem/randomevent/RandomEventZombieSwarm.java
@@ -4,7 +4,10 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import net.mitchfizz05.randomevents.eventsystem.EventDifficulty;
@@ -18,12 +21,22 @@ import net.mitchfizz05.randomevents.util.TimeHelper;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * A bunch of zombies spawn near the player, targeting them.
  */
 public class RandomEventZombieSwarm extends RandomEvent implements MobSpawner.IMobSpawnEvent
 {
+    private List<Item> helmets = new ArrayList<Item>() {{
+        add(Items.IRON_HELMET);
+        add(Items.GOLDEN_HELMET);
+        add(Items.DIAMOND_HELMET);
+        add(null);
+    }};
+
     public RandomEventZombieSwarm()
     {
         super("zombie_swarm");
@@ -48,11 +61,17 @@ public class RandomEventZombieSwarm extends RandomEvent implements MobSpawner.IM
     public Entity getEntity(World world, EntityPlayer player)
     {
         EntityZombie zombie = new EntityZombie(world);
+        zombie.replaceItemInInventory(103, new ItemStack(getHelmet(), 1));
         zombie.addPotionEffect(new PotionEffect(MobEffects.SPEED, 20 * 60 * 15)); // 15 minutes of speed.
         zombie.setBreakDoorsAItask(true); // Make doors breakable
         zombie.setAttackTarget(player); // Target the player
         zombie.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(80f); // Extend follow distance
 
         return zombie;
+    }
+
+    protected Item getHelmet()
+    {
+        return helmets.get(ThreadLocalRandom.current().nextInt(0, helmets.size()));
     }
 }
