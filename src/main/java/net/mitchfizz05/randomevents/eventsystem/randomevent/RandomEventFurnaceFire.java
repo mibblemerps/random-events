e33d73a5c1e758e0beb682d1f1fc950b6729955c
Mitchfizz05 <me@mitchfizz05.net>
2018-06-14 22:15:00 +0930

    Furnace fire and meltdown events happen way more often, because they will often fail because furnaces aren't running all the time.

--- src/main/java/net/mitchfizz05/randomevents/eventsystem/randomevent/RandomEventFurnaceFire.java
+++ src/main/java/net/mitchfizz05/randomevents/eventsystem/randomevent/RandomEventFurnaceFire.java
@@ -25,7 +25,7 @@ public class RandomEventFurnaceFire extends RandomEvent
     {
         super("furnace_fire");
 
-        addComponent(new CPlayerTimer(this, TimeHelper.hrsToSecs(0.5), TimeHelper.hrsToSecs(2.5)));
+        addComponent(new CPlayerTimer(this, TimeHelper.hrsToSecs(0), TimeHelper.hrsToSecs(0.33)));
         addComponent(new CPlayerEvent());
     }
 
